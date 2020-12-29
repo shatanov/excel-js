61299e13ab60a45752a22d44aee28c127bc34468
shatanov <timur-shatanov.work@mail.ru>
2020-12-29 19:16:24 +0300

    add listeners

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,17 @@
 export class DomListener {
+    constructor($root, listeners = []) {
+        if(!$root){
+            throw new Error('PROBLEM, SOS in DomListener');
+        }
+        this.$root = $root;
+        this.listeners = listeners;
+    }
 
+    initDomListener(){
+
+    }
+
+    removeDomListener(){
+
+    }
 }
